58d2cc2e9f570b9ffaca7cc1ddb45da1718291f8
MarinescuAna <marinescuana28@gmail.com>
2020-10-16 23:27:46 +0300

    sprint 1 - migrations and database creation

---
+++ Plush-API/Plush.DataAccessLayer.Repository/Migrations/20201016202258_Create_Database.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Plush.DataAccessLayer.Repository.Migrations
+{
+    public partial class Create_Database : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Ages = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Deliveries",
+                columns: table => new
+                {
+                    DeliveryID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Specification = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deliveries", x => x.DeliveryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Providers",
+                columns: table => new
+                {
+                    ProviderID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    ContactData = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Providers", x => x.ProviderID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fullname = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    AccessToken = table.Column<string>(nullable: true),
+                    AccessTokenExp = table.Column<DateTime>(nullable: true),
+                    Birthdate = table.Column<DateTime>(nullable: true),
+                    Role = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProviderDeliveries",
+                columns: table => new
+                {
+                    ProviderDeliveryID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProviderID = table.Column<int>(nullable: true),
+                    DeliveryID = table.Column<int>(nullable: true),
+                    Specification = table.Column<string>(nullable: true),
+                    DeliveryCompany = table.Column<string>(nullable: true),
+                    Price = table.Column<float>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProviderDeliveries", x => x.ProviderDeliveryID);
+                    table.ForeignKey(
+                        name: "FK_ProviderDeliveries_Deliveries_DeliveryID",
+                        column: x => x.DeliveryID,
+                        principalTable: "Deliveries",
+                        principalColumn: "DeliveryID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ProviderDeliveries_Providers_ProviderID",
+                        column: x => x.ProviderID,
+                        principalTable: "Providers",
+                        principalColumn: "ProviderID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Specification = table.Column<string>(nullable: true),
+                    Price = table.Column<float>(nullable: true),
+                    Stock = table.Column<int>(nullable: true),
+                    CategoryID = table.Column<int>(nullable: true),
+                    ProviderDeliveryID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductID);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryID",
+                        column: x => x.CategoryID,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Products_ProviderDeliveries_ProviderDeliveryID",
+                        column: x => x.ProviderDeliveryID,
+                        principalTable: "ProviderDeliveries",
+                        principalColumn: "ProviderDeliveryID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryID",
+                table: "Products",
+                column: "CategoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProviderDeliveryID",
+                table: "Products",
+                column: "ProviderDeliveryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProviderDeliveries_DeliveryID",
+                table: "ProviderDeliveries",
+                column: "DeliveryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProviderDeliveries_ProviderID",
+                table: "ProviderDeliveries",
+                column: "ProviderID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "ProviderDeliveries");
+
+            migrationBuilder.DropTable(
+                name: "Deliveries");
+
+            migrationBuilder.DropTable(
+                name: "Providers");
+        }
+    }
+}
